ffd944c49073ceaf7ce802cbac4781952663247c
Vadim <diesel010@ya.ru>
2024-07-05 21:44:09 +0300

    Commit_2

--- home_work/2_hw.py
+++ home_work/2_hw.py
@@ -1,9 +1,18 @@
-# def task_1(a:int,b:float,c:str,d:list):
-#     task_1(1,23.4,'test',[5,10,'двадцать пять'])
-#     return a, type(a),b,type(b),c, type(c),d,type(d)
-#     print(task_1)
+def task_1(a:int,b:float,c:str,d:list,e:bool)->list:
+        return (("a="),a, type(a),("целое число"),
+                ("b="),b, type(b),("число с плавающей точкой"),
+                ("c="),c, type(c), ("строка"),
+                ("d="),d, type(d),("список"),
+                ("e="),e, type(e)("логический тип"))
+print(task_1(1,23.4,'test',[5, 10, 'двадцать пять'],True))
+
+def task_2(a = [1, 2, 3, 5, 8, 13, 21])->list:
+        return a[0:3]
+print(task_2())
+task_2()
+# [1, 2, 3, 5, 8, 13, 21] Числа Фибоначчи
 
 
-def task_1(a:int,b:float):
-    return type(a),a,b, type(b)
-print(task_1(1,3.3))
+def task_3(x:float)->float:
+    return x*x
+print(task_3(5.45))
